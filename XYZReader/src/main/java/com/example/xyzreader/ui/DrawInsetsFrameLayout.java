1662ff86a5653869b8e1ee2ff0d6803a4a21bd90
mkillewald <mkillewald@yahoo.com>
2018-08-06 13:48:43 -0400

    removed unused code as suggested by Udacity code review

--- XYZReader/src/main/java/com/example/xyzreader/ui/DrawInsetsFrameLayout.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/DrawInsetsFrameLayout.java
@@ -30,9 +30,6 @@ import com.example.xyzreader.R;
 
 public class DrawInsetsFrameLayout extends FrameLayout {
     private Drawable mInsetBackground;
-    private Drawable mTopInsetBackground;
-    private Drawable mBottomInsetBackground;
-    private Drawable mSideInsetBackground;
 
     private Rect mInsets;
     private Rect mTempRect = new Rect();
